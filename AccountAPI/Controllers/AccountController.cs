8112587a818a0eb1ab40e93dc12da34f4ce8fa5c
Amit Sonker <amit.sonker@nagarro.com>
2019-11-11 13:09:55 +0530

    Data pushed

---
+++ AccountAPI/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void CreateAccount([FromBody] string customerData)
+        {
+            //do nothing
+        }
+
+        [HttpPost]
+        public void UpdateAccount([FromBody] string customerData)
+        {
+            //do nothing
+        }
+
+        [HttpGet]
+        public IList<string> GetAccounts(string userId)
+        {
+            return new List<string>();
+            //do nothing
+        }
+
+
+        [HttpPost]
+        public void RemoveAccount([FromBody] string accountId)
+        {
+            //do nothing
+        }
+
+        [HttpPost]
+        public void DepositMoneyToAccount([FromBody] double amount, string accountNumber)
+        {
+            //do nothing
+        }
+
+        [HttpPost]
+        public void WithdrawMoneyFromAccount([FromBody] double amount, [FromBody] string accountNumber)
+        {
+            //do nothing
+        }
+
+        [HttpPost]
+        public void TransferMoneyFromAccountToAnotherAccount([FromBody] double amount, [FromBody] string accountNumber, [FromBody] string receiverAccountNumber)
+        {
+            //do nothing
+        }
+
+    }
+}
--- MicroServicesAssignment/Controllers/CustomerController.cs
+++ MicroServicesAssignment/Controllers/CustomerController.cs
@@ -21,5 +21,31 @@ namespace MicroServicesAssignment.Controllers
         {
             return Content(configSettings.Message);
         }
+
+        [HttpPost]
+        public void RegisterCustomer([FromBody] string customerData)
+        {
+            //do nothing
+        }
+
+        [HttpPost]
+        public void UpdateCustomerInfo([FromBody] string customerData)
+        {
+            //do nothing
+        }
+
+
+        [HttpPost]
+        public void OrderChequeBook()
+        {
+            //do nothing
+        }
+
+
+        [HttpPost]
+        public void CancelChequeBook()
+        {
+            //do nothing
+        }
     }
 }
