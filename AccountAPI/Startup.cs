5a798b950769258972e501c3d2bf3ad56ec72d8e
Amit Sonker <amit.sonker@nagarro.com>
2019-11-11 13:57:53 +0530

    Code added

--- AccountAPI/Startup.cs
+++ AccountAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.Discovery.Client;
 
 namespace AccountAPI
 {
@@ -25,6 +26,7 @@ namespace AccountAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDiscoveryClient(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -35,11 +37,7 @@ namespace AccountAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
-
+            app.UseDiscoveryClient();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
